0e38e6e5b65de5324af2d11a65a1a5a2ed05b00b
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-09-28 16:10:18 -0400

    java and index.html

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 console.log("I'm connected");
+var startButton = document.querySelector("#start-button");
+var guessAnswer = document.querySelector("#guess-answer");
+var rightA = document.querySelector("#right");
+var wrongA = document.querySelector("#wrong");
+var timerA = document.querySelector("#timer");
+
+var AnswerTrack = [];
+var WrongAnswer = 0;
+var totalWins = 0;
+var totalLosses = 0;
+var timerInterval = null;
+var secondsLeft = 0;
+
+// If user refresh page, It will pick up where left off
+if(localStorage.getItem("totalWins") !== null){
+    totalWins = parseInt(localStorage.getItem("totalWins") );
+    rightA.innerHTML = "Right: " + totalWins;
+}
+if(localStorage.getItem("totalLosses") !== null){
+    totalLosses = parseInt(localStorage.getItem("totalLosses") );
+    wrongA.innerHTML = "wrong: " + totalLosses;
+}
+
+
 
 var imgTag = document.createElement("img");
 
