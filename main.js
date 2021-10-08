46c9555122bdb1837ecf1f779135ba9d79185a1e
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-10-08 19:30:47 -0400

    java

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 console.log("I'm connected");
+var startButton = document.querySelector("#start-button");
+var guessWordPre = document.querySelector("#answer");
+var winsP = document.querySelector("#correct");
+var lossesP = document.querySelector("#incorrect");
+var timerP = document.querySelector("#timer");
+
+var answerTrack = [];
+var wrongAnswer = 0;
+var totalCorrect = 0;
+var totalIncorrect = 0;
+var timerInterval = null;
+var secLeft = 0;
+// When a user refreshers or returns to the browser page
+if(localStorage.getItem("totalCorrect") !== null){
+    totalWins = parseInt(localStorage.getItem("totalCorrect") );
+    winsP.innerHTML = "Correct: " + totalCorrect;
+}
+if(localStorage.getItem("totalIncorrect") !== null){
+    totalLosses = parseInt(localStorage.getItem("totalIncorrect") );
+    lossesP.innerHTML = "incorrect: " + totalLosses;
 
 var imgTag = document.createElement("img");
 
